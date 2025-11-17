56b3e5ce2db65c33c53f255c3f0159b73ea032e8
muhammadhabib16 <2411522024_muhammad@student.unand.ac.id>
2025-11-17 22:08:15 +0700

    third commit

--- src/components/DynamicBreadcrumb.jsx
+++ src/components/DynamicBreadcrumb.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
-import Breadcrumb from "./BreadCrumb";
+import Breadcrumb from "./Breadcrumb";
 import { useAuth } from "../context/AuthContext";
 
 export default function DynamicBreadcrumb() {
